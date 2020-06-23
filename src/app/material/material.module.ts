bb862fe6b13ddd2e5570abb3b0ab99d325f1cb42
Isaac Olguin <misael.olguin@gmail.com>
2020-06-22 21:37:30 -0500

    Actualizacion de interface para productos, header y footer

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,17 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatCardModule
   ],
   exports: [
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
